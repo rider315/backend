98a9a15f87721936df94bc9affea20f40da5cf28
Gaurav Chaudhary <jaatsaab735@gmail.com>
2024-12-14 18:22:00 +0530

    removed the comments

--- models/Seat.js
+++ models/Seat.js
@@ -1,47 +1,8 @@
-// const mongoose = require("mongoose");
-
-// const seatSchema = new mongoose.Schema({
-//   id: { type: String, required: true },  // Seat number
-//   isBooked: { type: Boolean, default: false }  // Booking status
-// });
-
-// module.exports = mongoose.model("Seat", seatSchema);
-
-
-// const mongoose = require("mongoose");
-
-// const seatSchema = new mongoose.Schema({
-//   id: { type: String, required: true },
-//   isBooked: { type: Boolean, default: false },
-// });
-
-// const Seat = mongoose.model("Seat", seatSchema);
-
-// module.exports = Seat;
-
-
-// const mongoose = require("mongoose");
-
-// const seatSchema = new mongoose.Schema({
-//   id: {
-//     type: String,
-//     required: true,
-//   },
-//   isBooked: {
-//     type: Boolean,
-//     default: false,  // Initially, all seats are not booked
-//   },
-// });
-
-// const Seat = mongoose.model("Seat", seatSchema);
-
-// module.exports = Seat;
-
 const mongoose = require("mongoose");
 
 const seatSchema = new mongoose.Schema({
   seatId: {
-    type: String,  // Change _id to seatId as string
+    type: String,  
     required: true,
     unique: true
   },
@@ -49,7 +10,7 @@ const seatSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
-  // Other seat properties can go here
+  
 });
 
 const Seat = mongoose.model("Seat", seatSchema);
